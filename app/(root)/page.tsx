a21132f556c3312ff5d097b91bedcad6d13a8f93
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-07-05 10:02:38 +0530

    minor changes done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,5 +16,5 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Home;
 
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+
+const TransfromationIdPage = () => {
+    
+  return (
+
+    <div>TransfromationIdPage</div>
+  )
+}
+
+export default TransfromationIdPage
