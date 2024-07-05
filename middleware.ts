2b2503c81a9eac0cb74c36a1eb573ef6a3b2da14
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-07-05 10:50:18 +0530

    sync clerk data to your backend via webhooks

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,13 @@
-
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server';
 
 export default clerkMiddleware();
 
 export const config = {
-
-  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-
+  matcher: [
+    '/((?!.*\\..*|_next|api/webhooks/.*).*)', 
+    '/', 
+    '/(api|trpc)(.*)'
+  ],
 };
 
 
