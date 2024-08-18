14a4939b5b992b0fe10d8ad0466e6f43f8d2eea6
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-08-18 07:38:50 +0530

    minor changes

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -53,26 +53,26 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
 
-    const user = {
-      clerkId: id || "",
-      email: email_addresses[0].email_address || "",
-      userName: username!,  // Updated to "userName"
-      firstName: first_name || "",
-      lastName: last_name || "",
-      photo: image_url,
-    };
+    // const user = {
+    //   clerkId: id || "",
+    //   email: email_addresses[0].email_address || "",
+    //   userName: username!,  // Updated to "userName"
+    //   firstName: first_name || "",
+    //   lastName: last_name || "",
+    //   photo: image_url,
+    // };
 
-    const newUser = await createUser(user);
+    // const newUser = await createUser(user);
 
-    if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
-        publicMetadata: {
-          userId: newUser._id,
-        },
-      });
-    }
+    // if (newUser) {
+    //   await clerkClient.users.updateUserMetadata(id, {
+    //     publicMetadata: {
+    //       userId: newUser._id,
+    //     },
+    //   });
+    // }
 
-    return NextResponse.json({ message: "OK", user: newUser });
+    return NextResponse.json({ message: "OK", user: evt.data });
   }
 
   // UPDATE
