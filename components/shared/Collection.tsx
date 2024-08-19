2f3847ea3a3e77186a83e0c87c8ddf05df99042f
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-08-19 13:41:38 +0530

    final deploy

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -55,8 +55,8 @@ export const Collection = ({
 
       {images.length > 0 ? (
         <ul className="collection-list">
-          {images.map((image) => (
-            <Card image={image} key={image._id} />
+          {images.map((image,index) => (
+            <Card image={image} key={index} />
           ))}
         </ul>
       ) : (
